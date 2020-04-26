8da394b0f1237f059e39c6413e9e3a7f1fe6ba58
Yoon_Koh <yk1803@nyu.edu>
2020-04-26 17:59:03 +0900

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Users from './Users';
 
 import ResourceList from './ResourceList';
 
-class App extends React.Component {
-  state = {
-    resourceName: 'posts'
-  }
+// class App extends React.Component {
+//   state = {
+//     resourceName: 'posts'
+//   }
 
-  render() {
-    return (
-      <React.Fragment>
-        <button onClick={() => this.setState({ resourceName: 'posts' })}>Posts</button>
-        <button onClick={() => this.setState({ resourceName: 'todos' })}>Todos</button>
-        <ResourceList resourceName={this.state.resourceName}/>
-      </React.Fragment>
-    )
-  }
-}
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <button onClick={() => this.setState({ resourceName: 'posts' })}>Posts</button>
+//         <button onClick={() => this.setState({ resourceName: 'todos' })}>Todos</button>
+//         <ResourceList resourceName={this.state.resourceName}/>
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+const App = () => {
+	const [resourceName, setResourceName] = useState('posts');
+	console.log('sex', resourceName);
+	console.log(setResourceName);
+	return (
+		<React.Fragment>
+			<Users />
+			<button onClick={() => setResourceName('posts')}>Posts</button>
+			<button onClick={() => setResourceName('todos')}>Todos</button>
+			<ResourceList resourceName={resourceName} />
+		</React.Fragment>
+	);
+};
 export default App;
---
+++ src/components/Users.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import useResources from './useResources';
+
+const Users = () => {
+	const users = useResources('users');
+	return (
+		<ul>
+			{users.map((user) => (
+				<li key={user.id}>{user.name}</li>
+			))}
+		</ul>
+	);
+};
+
+export default Users;
